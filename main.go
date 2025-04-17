2c4121531fdf60f9e493d550a0a9166201c59f31
1111 <a1111@MacBook-Air-1111.local>
2025-04-17 05:55:09 +0500

    Add PATCH and DELETE handlers for tasks

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var task string
+type Task struct {
+	ID string `json:"id"`
+	Task string `json:"task"`
+}
+
+var tasks []Task
 
 type requestBody struct {
 	Task string `json:"task"`
@@ -24,18 +30,62 @@ func postHandler(c echo.Context) error {
 
 	}
 
-	task = body.Task
+	newTask := Task {
+		ID: uuid.NewString(),
+		Task: body.Task,
+	}
 
-	response := fmt.Sprintf("Task updated: %s", task)
+	tasks = append(tasks, newTask)
+
+	response := fmt.Sprintf("Task updated: %s", newTask.ID)
 
 	return c.JSON(http.StatusOK, map[string]string{"message": response})
 }
 
 func getHandler(c echo.Context) error {
+    id := c.Param("id")
 
-	response := fmt.Sprintf("hello, %s", task)
+	for _, task := range tasks {
+		if task.ID == id {
+			return c.JSON(http.StatusOK, task)
+		}
+	}
+	
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "Task not found"})
+}
 
-	return c.JSON(http.StatusOK, map[string]string{"message": response})
+func pathHandler(c echo.Context) error {
+	id := c.Param("id")
+    
+	var body requestBody
+	err := json.NewDecoder(c.Request().Body).Decode(&body)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	for i, task := range tasks {
+		if task.ID == id {
+			if body.Task != "" {
+				tasks[i].Task = body.Task
+			}
+			return c.JSON(http.StatusOK, tasks[i])
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "Task not found"})
+}
+
+func deleteHandler(c echo.Context) error {
+	id := c.Param("id")
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return c.JSON(http.StatusOK, map[string]string{"message": "Task deleted"})
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "Task not found"})
 }
 
 func main() {
@@ -44,8 +94,10 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
 
-	e.POST("/post", postHandler)
-	e.GET("/get", getHandler)
+	e.POST("/tasks", postHandler)
+	e.GET("/tasks/:id", getHandler)
+	e.PATCH("/tasks/:id", pathHandler)
+	e.DELETE("/tasks/:id", deleteHandler)
 
 	e.Start("localhost:8080")
 }
